322037c0aebc4cdd20344503e5d4d0046335603a
gabyceci <20190019@ricaldone.edu.sv>
2025-03-13 15:08:38 -0600

    3 CRUD mas

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 //Importo todo lo de la libreria de Express
 import express from "express";
 import productRoutes from "./src/routes/products.js"
+import branchesRoutes from "./src/routes/branches.js"
+import clientsRoutes from "./src/routes/clients.js"
+import employeesRoutes from "./src/routes/employees.js"
+
 
 //Creo una constante que es igual a la libreria que importé
 const app = express();
@@ -10,6 +14,14 @@ app.use(express.json());
 
 //Definir las rutas de las funciones que tendrá la página web
 app.use("/api/products", productRoutes);
+
+app.use("/api/branches", branchesRoutes);
+
+app.use("/api/clients", clientsRoutes);
+
+app.use("/api/employees", employeesRoutes);
+
+
 
 //Exporto la constante para poder usar express en otros archivos
 export default app;
---
+++ backend/src/models/Employees.js
@@ -0,0 +1,65 @@
+/*
+Campos:
+name
+latName
+birthday
+address
+hireDate
+password
+telephone
+dui
+isssNumber
+isVerified
+*/
+
+import { Schema, model } from "mongoose";
+
+const employeesSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+    },
+    birthday: {
+      type: Date,
+      require: true,
+    },
+    address: {
+      type: String,
+      require: true,
+    },
+    hireDate: {
+      type: Date,
+      require: true,
+    },
+    password: {
+        type: String,
+        require: true,
+      },
+    telephone: {
+      type: String,
+      require: true,
+    },
+    dui: {
+      type: Number,
+      require: true,
+    },
+    isssNumber: {
+        type: Number,
+        require: true,
+      },
+    isVerified: {
+      type: Boolean,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+    strict: false,
+  }
+);
+
+export default model("Employees", employeesSchema);
